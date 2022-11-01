8a624d71fffae7ff57c103d4eb288107eb336197
alaia <alaiatorre2001@gmail.com>
2022-11-01 12:00:16 +0100

    Argazkiak ezabatuta

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-// fetch('http://ikasten.io:3000/https://aenui.org/actas/indice_e.html#arribaDelTodo').then(r => r.text()).then(r => document.write(r))
- //fetch('http://ikasten.io:3000/https://aenui.org/actas/indice_e.html').then(r => r.json()).then(r => console.log(r))
  fetch("http://ikasten.io:3000/https://aenui.org/actas/indice_e.html")
     .then(response => response.text())
     .then(r => {
         r=r.replace('(src=")', 'src="https://aenui.org/actas/')
         r=r.replace('href="','href="https://aenui.org/actas/')
         document.getElementsByTagName("html")[0].innerHTML=r
+
+        document.querySelectorAll(".portada img")
+        .forEach(img => img.remove());
+
     })
-let proba=document.getElementById("id_jenui_1997")
-console.log(proba)
+/*let proba=document.getElementById("id_jenui_1997")
+console.log(proba)*/
